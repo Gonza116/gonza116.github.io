c7364e989679c1e7ad1480fb2a6726d9f1aceef3
Gonzalo Garcia Granés <ggarcia@quebueno.es>
2022-10-27 12:29:28 +0200

    Add music tops section

---
+++ src/constants/musicTops.js
@@ -0,0 +1,754 @@
+export const twentyOne = {
+	year: "2021",
+	albums: [
+		{
+			position: 1,
+			artist: "Billie Eillish",
+			name: "Happier Than Ever",
+			cover:
+				"https://media.revistagq.com/photos/608a6889235a5910299c8f00/master/pass/Billie%20Eilish_cover%20album%20Happier%20Than%20Ever.jpg",
+		},
+		{
+			position: 2,
+			artist: "Spellling",
+			name: "The Turning Wheel",
+			cover: "https://m.media-amazon.com/images/I/51oZ7VoqtgL._SL1500_.jpg",
+		},
+		{
+			position: 3,
+			artist: "Tyler, The Creator",
+			name: "CALL ME IF YOU GET LOST",
+			cover:
+				"https://indiehoy.com/wp-content/uploads/2021/08/tyler-the-creator-call-me-if-you-get-lost.jpg",
+		},
+		{
+			position: 4,
+			artist: "Zahara",
+			name: "PUTA",
+			cover:
+				"https://www.lahiguera.net/musicalia/artistas/zahara/disco/11125/zahara_puta-portada.jpg",
+		},
+		{
+			position: 5,
+			artist: "Olivia Rodrigo",
+			name: "SOUR",
+			cover:
+				"https://uproxx.com/wp-content/uploads/2021/11/artworks-ordNOXxSymLRo3kU-2tpxUQ-t3000x3000.jpg",
+		},
+		{
+			position: 6,
+			artist: "Kacey Musgraves",
+			name: "star-crossed",
+			cover:
+				"https://media.pitchfork.com/photos/6123c663107002f32deb7762/master/pass/Kacey-Musgraves-Star-Crossed.jpeg",
+		},
+		{
+			position: 7,
+			artist: "Bleachers",
+			name: "Take The Saddness Out Of Saturday Night",
+			cover: "https://m.media-amazon.com/images/I/A16C3SdDpwL._SL1500_.jpg",
+		},
+		{
+			position: 8,
+			artist: "Porter Robinson",
+			name: "Nurture",
+			cover: "https://m.media-amazon.com/images/I/81qzh5RIv8L._SL1200_.jpg",
+		},
+		{
+			position: 9,
+			artist: "La M.O.D.A.",
+			name: "Nuevo Cancionero Burgalés",
+			cover:
+				"https://www.lahiguera.net/musicalia/artistas/la_maravillosa_orquesta_del_alcohol/disco/11664/la_maravillosa_orquesta_del_alcohol_nuevo_cancionero_burgales-portada.jpg",
+		},
+		{
+			position: 10,
+			artist: "Zara Larsson",
+			name: "Poster Girl",
+			cover:
+				"https://i.discogs.com/ObF_U6T9F0ZZb3rmPEpp8y6hSwP7Hnx3caSL1-LhJPU/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE3NzA3/OTQ4LTE2MTQ5NzU1/NjgtMjY0Ni5qcGVn.jpeg",
+		},
+		{
+			position: 11,
+			artist: "Tu Suerte",
+			name: "Esto Es Para Ti",
+			cover:
+				"https://www.mondosonoro.com/wp-content/uploads/2021/11/tu-suerte-disco.jpg",
+		},
+		{
+			position: 12,
+			artist: "Little Simz",
+			name: "Sometimes I Might Be Introvert",
+			cover:
+				"https://i.discogs.com/6HwBif-XJNcgDnYu_pOMoYAL9pgvMW9b416L2eOHgDQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwMDY2/OTQ3LTE2MzA4NDAy/NjEtNzI2Mi5qcGVn.jpeg",
+		},
+		{
+			position: 13,
+			artist: "No Rome",
+			name: "It's All Smiles",
+			cover:
+				"https://i.discogs.com/QhEYQvIl5KRUOqHf99G5dlAFQDPVBB5iCzxoaTsIH54/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIzNzk0/Njk0LTE2NTcwNjA4/MTItNjczMi5wbmc.jpeg",
+		},
+		{
+			position: 14,
+			artist: "FINNEAS",
+			name: "Optimist",
+			cover:
+				"https://i.discogs.com/eURhs96XvZMXWQtrXZtodSxp7TD2wQifZ0z_r08Dq-w/rs:fit/g:sm/q:90/h:542/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwNTg0/NjQ1LTE2MzQyOTI2/OTMtMjMwMy5qcGVn.jpeg",
+		},
+		{
+			position: 15,
+			artist: "Chvrches",
+			name: "Screen Violence",
+			cover:
+				"https://i.discogs.com/9ymgA_EliwxUVv9dO_0qaGFPsLmVYbG9g_Lr2BX6960/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIwMDEw/OTUyLTE2MzAwNTY4/MjYtOTU5MC5qcGVn.jpeg",
+		},
+		{
+			position: 16,
+			artist: "Villagers",
+			name: "Fever Dreams",
+			cover:
+				"https://i.discogs.com/Sh785yBtwBYvqjXiW7Zfu5gukWykuInN_Hs0DhOs-jE/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTIxNTkz/MzQ3LTE2NDEyNDIw/NzMtMTIzNS5qcGVn.jpeg",
+		},
+		{
+			position: 17,
+			artist: "Lucy Dacus",
+			name: "Home Video",
+			cover:
+				"https://i.discogs.com/uFEbL9Tk3dl5lMh-brspqdTEiu5WxV1vDi57sHe9D70/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE5Mjg1/ODI4LTE2MjQ3MjY3/OTktMTExMy5qcGVn.jpeg",
+		},
+		{
+			position: 18,
+			artist: "Alavedra",
+			name: "Uf Venga",
+			cover:
+				"https://www.mondosonoro.com/wp-content/uploads/2021/10/alavedra-uf-venga.jpg",
+		},
+		{
+			position: 19,
+			artist: "Jónsi",
+			name: "Obsidian",
+			cover: "https://i.scdn.co/image/ab67616d0000b2730013164a8f83c7ff5469fe55",
+		},
+		{
+			position: 20,
+			artist: "Japanese Breakfast",
+			name: "Jubilee",
+			cover:
+				"https://i.discogs.com/UE3CHjMVlzCGMLNz-JCCh1cNb-ayH4fokcZVm8SRzRc/rs:fit/g:sm/q:90/h:592/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4OTg4/MDY5LTE2MjI3NTI2/MTktODI4MS5qcGVn.jpeg",
+		},
+		{
+			position: 21,
+			artist: "Chloe Moriondo",
+			name: "Blood Bunny",
+			cover:
+				"https://i.discogs.com/M7BhQebgTj2TGnbBehkAS0glShnGvGsrOeEPOTlfjEU/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE4NjQ0/OTk4LTE2MjA0OTU0/MjgtMTMzOS5qcGVn.jpeg",
+		},
+		{
+			position: 22,
+			artist: "Easy Life",
+			name: "life's a beach",
+			cover: "https://m.media-amazon.com/images/I/71CTDCxhXwL._SL1400_.jpg",
+		},
+		{
+			position: 23,
+			artist: "Arlo Parks",
+			name: "Collapsed In Sunbeams",
+			cover: "https://m.media-amazon.com/images/I/711gakOQisL._SL1500_.jpg",
+		},
+		{
+			position: 24,
+			artist: "PinkPantheress",
+			name: "to hell with it",
+			cover:
+				"https://m.media-amazon.com/images/I/51nhH+LH5mL._UXNaN_FMjpg_QL85_.jpg",
+		},
+		{
+			position: 25,
+			artist: "Coldplay",
+			name: "Music Of The Spheres",
+			cover: "https://m.media-amazon.com/images/I/71bR67Ia7YL._SL1425_.jpg",
+		},
+	],
+};
+
+export const twenty = {
+	year: "2020",
+	albums: [
+		{
+			position: 1,
+			artist: "Phoebe Bridgers",
+			name: "Punisher",
+			cover:
+				"https://pbs.twimg.com/media/EqgETj5W8AYFqxX?format=jpg&name=medium",
+		},
+		{
+			position: 2,
+			artist: "Taylor Swift",
+			name: "folklore",
+			cover:
+				"https://pbs.twimg.com/media/EqgDPZ-XYAM969X?format=png&name=medium",
+		},
+		{
+			position: 3,
+			artist: "The Strokes",
+			name: "The New Abnormal",
+			cover:
+				"https://pbs.twimg.com/media/EqgDxBZXUAEpb79?format=jpg&name=medium",
+		},
+		{
+			position: 4,
+			artist: "Jónsi",
+			name: "Shiver",
+			cover:
+				"https://pbs.twimg.com/media/EqgCkUcXAAEP-zm?format=jpg&name=medium",
+		},
+		{
+			position: 5,
+			artist: "Viva Belgrado",
+			name: "Bellavista",
+			cover:
+				"https://pbs.twimg.com/media/EqgCHnCXMAc2s6w?format=jpg&name=medium",
+		},
+		{
+			position: 6,
+			artist: "Taylor Swift",
+			name: "evermore",
+			cover:
+				"https://pbs.twimg.com/media/EqgB1akXIAMqdsZ?format=jpg&name=medium",
+		},
+		{
+			position: 7,
+			artist: "Declan McKenna",
+			name: "Zeros",
+			cover:
+				"https://pbs.twimg.com/media/EqgBaCWW4AAT6Zi?format=jpg&name=medium",
+		},
+		{
+			position: 8,
+			artist: "The Koreatown Oddity",
+			name: "Little Dominiques Nosebleed",
+			cover:
+				"https://pbs.twimg.com/media/EqgA6bAW8AAfPgS?format=png&name=small",
+		},
+		{
+			position: 9,
+			artist: "The Avalanches",
+			name: "We Will Always Love You",
+			cover:
+				"https://pbs.twimg.com/media/EqgAYrMXEAIKjPO?format=png&name=small",
+		},
+		{
+			position: 10,
+			artist: "Against All Logic",
+			name: "2017-2019",
+			cover:
+				"https://pbs.twimg.com/media/Eqf_wr5XEAQKfbi?format=jpg&name=medium",
+		},
+		{
+			position: 11,
+			artist: "Bad Bunny",
+			name: "Yo Hago Lo Que Me Da La Gana",
+			cover:
+				"https://pbs.twimg.com/media/Eqf_anzWMAU3aHZ?format=jpg&name=medium",
+		},
+		{
+			position: 12,
+			artist: "Nothing But Thieves",
+			name: "Moral Panic",
+			cover:
+				"https://pbs.twimg.com/media/Eqf-uNEXEAAwWLg?format=jpg&name=medium",
+		},
+		{
+			position: 13,
+			artist: "Charli XCX",
+			name: "how i'm feeling now",
+			cover:
+				"https://pbs.twimg.com/media/Eqf-dmTXYAogabO?format=jpg&name=medium",
+		},
+		{
+			position: 14,
+			artist: "beabadoobee",
+			name: "Fake It Flowers",
+			cover:
+				"https://pbs.twimg.com/media/Eqf93aRW8AMma_a?format=jpg&name=medium",
+		},
+		{
+			position: 15,
+			artist: "Black Pumas",
+			name: "Black Pumas",
+			cover:
+				"https://pbs.twimg.com/media/Eqf87XKXcAIuQT1?format=jpg&name=medium",
+		},
+		{
+			position: 16,
+			artist: "HAIM",
+			name: "Women In Music Part III",
+			cover:
+				"https://pbs.twimg.com/media/Eqf7bIkXYAAE7Qw?format=jpg&name=medium",
+		},
+		{
+			position: 17,
+			artist: "Fiona Apple",
+			name: "Fetch The Bolt Cutters",
+			cover:
+				"https://pbs.twimg.com/media/Eqf7CmuXAAQBWYh?format=jpg&name=medium",
+		},
+		{
+			position: 18,
+			artist: "Khruangbin",
+			name: "Mordecai",
+			cover:
+				"https://pbs.twimg.com/media/Eqf6xP3XIAcqjIv?format=jpg&name=medium",
+		},
+		{
+			position: 19,
+			artist: "Cave People",
+			name: "Looking",
+			cover:
+				"https://pbs.twimg.com/media/Eqf6h0dWMAE17zm?format=jpg&name=medium",
+		},
+		{
+			position: 20,
+			artist: "La M.O.D.A.",
+			name: "Ninguna Ola",
+			cover:
+				"https://pbs.twimg.com/media/Eqf6OYtXIAEvNmA?format=jpg&name=medium",
+		},
+		{
+			position: 21,
+			artist: "King Gizzard & The Lizard Wizard",
+			name: "K.G.",
+			cover:
+				"https://pbs.twimg.com/media/Eqf5icwWMAIxaZK?format=png&name=small",
+		},
+		{
+			position: 22,
+			artist: "Will Butler",
+			name: "Generations",
+			cover:
+				"https://pbs.twimg.com/media/Eqf5J6DXcAEtFtA?format=png&name=900x900",
+		},
+		{
+			position: 23,
+			artist: "Christine And The Queens",
+			name: "La Vita Nuova",
+			cover:
+				"https://pbs.twimg.com/media/Eqf41wCXMAMefS8?format=png&name=small",
+		},
+		{
+			position: 24,
+			artist: "Gorillaz",
+			name: "Song Machine, Season 1: Strange Timez",
+			cover:
+				"https://pbs.twimg.com/media/Eqf4fd6XMAEQOGT?format=jpg&name=medium",
+		},
+		{
+			position: 25,
+			artist: "Nathy Peluso",
+			name: "CALAMBRE",
+			cover:
+				"https://pbs.twimg.com/media/Eqf4H8-XAAEH1QY?format=jpg&name=medium",
+		},
+	],
+};
+
+export const nineteen = {
+	year: "2019",
+	albums: [
+		{
+			position: 1,
+			artist: "Caroline Polacheck",
+			name: "Pang",
+			cover:
+				"https://pbs.twimg.com/media/ENIkQyfXUAAWwEt?format=jpg&name=medium",
+		},
+		{
+			position: 2,
+			artist: "Charli XCX",
+			name: "Charli",
+			cover:
+				"https://pbs.twimg.com/media/ENIXr-2WwAEzO7o?format=jpg&name=medium",
+		},
+		{
+			position: 3,
+			artist: "American Football",
+			name: "LP3",
+			cover:
+				"https://pbs.twimg.com/media/ENIjMazWkAAJsGn?format=jpg&name=medium",
+		},
+		{
+			position: 4,
+			artist: "FOALS",
+			name: "Everything Not Saved Will Be Lost Part I",
+			cover:
+				"https://pbs.twimg.com/media/ENIiKd-WwAEnKv1?format=jpg&name=medium",
+		},
+		{
+			position: 5,
+			artist: "La Casa Azul",
+			name: "La Gran Esfera",
+			cover:
+				"https://pbs.twimg.com/media/ENIhiKMXUAAHiW9?format=png&name=900x900",
+		},
+		{
+			position: 6,
+			artist: "Vampire Weekend",
+			name: "Father Of The Bride",
+			cover:
+				"https://pbs.twimg.com/media/ENIg_CKW4AAOZgP?format=jpg&name=medium",
+		},
+		{
+			position: 7,
+			artist: "Of Monsters And Men",
+			name: "Fever Dream",
+			cover:
+				"https://pbs.twimg.com/media/ENIgTQsWkAYwRuO?format=jpg&name=medium",
+		},
+		{
+			position: 8,
+			artist: "Friendly Fires",
+			name: "Innocent",
+			cover:
+				"https://pbs.twimg.com/media/ENIfrfcWoAAb3eg?format=jpg&name=medium",
+		},
+		{
+			position: 9,
+			artist: "SYML",
+			name: "SYML",
+			cover:
+				"https://pbs.twimg.com/media/ENIfIszWwAEFOM8?format=png&name=medium",
+		},
+		{
+			position: 10,
+			artist: "The Lumineers",
+			name: "III",
+			cover:
+				"https://pbs.twimg.com/media/ENIekIHWoAYRhEW?format=png&name=900x900",
+		},
+		{
+			position: 11,
+			artist: "Coldplay",
+			name: "Everyday Life",
+			cover:
+				"https://pbs.twimg.com/media/ENIdvs3WsAEKFPF?format=jpg&name=medium",
+		},
+		{
+			position: 12,
+			artist: "The Chemical Brothers",
+			name: "No Geography",
+			cover:
+				"https://pbs.twimg.com/media/ENIbvmvWoAA7vl8?format=jpg&name=medium",
+		},
+		{
+			position: 13,
+			artist: "Tiny Moving Parts",
+			name: "breathe",
+			cover:
+				"https://pbs.twimg.com/media/ENIbPC-XUAEdfQ6?format=jpg&name=medium",
+		},
+		{
+			position: 14,
+			artist: "Harry Styles",
+			name: "Fine Line",
+			cover:
+				"https://pbs.twimg.com/media/ENIaihWXYAQUuJr?format=jpg&name=medium",
+		},
+		{
+			position: 15,
+			artist: "Avey Tare",
+			name: "Cows On Hourglass Pond",
+			cover:
+				"https://pbs.twimg.com/media/ENIZha0WsAAtQkD?format=jpg&name=medium",
+		},
+		{
+			position: 16,
+			artist: "The Regrettes",
+			name: "How Do You Love?",
+			cover:
+				"https://pbs.twimg.com/media/ENIY0AxXUAAAP7l?format=jpg&name=medium",
+		},
+		{
+			position: 17,
+			artist: "half·alive",
+			name: "Now, Not Yet",
+			cover:
+				"https://pbs.twimg.com/media/ENIYVvMXkAAfeJi?format=jpg&name=medium",
+		},
+		{
+			position: 18,
+			artist: "The S.L.P.",
+			name: "The S.L.P.",
+			cover:
+				"https://pbs.twimg.com/media/ENIW-NZWkAAbiF5?format=jpg&name=medium",
+		},
+		{
+			position: 19,
+			artist: "Mark Ronson",
+			name: "Late Night Feelings",
+			cover:
+				"https://pbs.twimg.com/media/ENIWefyXUAAl9s1?format=png&name=small",
+		},
+		{
+			position: 20,
+			artist: "Kanye West",
+			name: "Jesus Is King",
+			cover:
+				"https://pbs.twimg.com/media/ENIV6w2XkAAeJFF?format=jpg&name=medium",
+		},
+		{
+			position: 21,
+			artist: "FOALS",
+			name: "Everything Not Saved Will Be Lost Part II",
+			cover:
+				"https://pbs.twimg.com/media/ENIVW7uWsAM5229?format=jpg&name=medium",
+		},
+		{
+			position: 22,
+			artist: "Taylor Swift",
+			name: "Lover",
+			cover:
+				"https://pbs.twimg.com/media/ENIU0_jWkAEZ8_I?format=jpg&name=medium",
+		},
+		{
+			position: 23,
+			artist: "Amaia",
+			name: "Pero No Pasa Nada",
+			cover:
+				"https://pbs.twimg.com/media/ENIUPySXUAUh96I?format=jpg&name=medium",
+		},
+		{
+			position: 24,
+			artist: "Cage The Elephant",
+			name: "Social Cues",
+			cover:
+				"https://pbs.twimg.com/media/ENITiXIWsAEveDv?format=jpg&name=medium",
+		},
+		{
+			position: 25,
+			artist: "The Amazons",
+			name: "Future Dust",
+			cover:
+				"https://pbs.twimg.com/media/ENIS0I9XsAEMGfL?format=jpg&name=medium",
+		},
+	],
+};
+
+export const eighteen = {
+	year: "2018",
+	albums: [
+		{
+			position: 1,
+			artist: "Rosalía",
+			name: "El Mal Querer",
+			cover:
+				"https://pbs.twimg.com/media/DvwTMzZWkAE5kUB?format=jpg&name=medium",
+		},
+		{
+			position: 2,
+			artist: "First Aid Kit",
+			name: "Ruins",
+			cover:
+				"https://pbs.twimg.com/media/DvwT4JZX0AIi8i0?format=jpg&name=medium",
+		},
+		{
+			position: 3,
+			artist: "Against All Logic",
+			name: "2012-2017",
+			cover:
+				"https://pbs.twimg.com/media/DvwSgQAXgAE2OfT?format=jpg&name=medium",
+		},
+		{
+			position: 4,
+			artist: "The 1975",
+			name: "A Brief Inquiry Into Online Relationships",
+			cover:
+				"https://upload.wikimedia.org/wikipedia/en/2/2d/A_Brief_Inquiry_into_Online_Relationships.png",
+		},
+		{
+			position: 5,
+			artist: "Zahara",
+			name: "Astronauta",
+			cover:
+				"https://cdns-images.dzcdn.net/images/cover/6b9802fa33834cbef6448799717191d6/500x500.jpg",
+		},
+		{
+			position: 6,
+			artist: "KIDS SEE GHOSTS",
+			name: "KIDS SEE GHOSTS",
+			cover: "https://m.media-amazon.com/images/I/81vqCvM2EJL._SL1200_.jpg",
+		},
+		{
+			position: 7,
+			artist: "Tropical Fuck Storm",
+			name: "A Bleeding Death In Meatspace",
+			cover:
+				"https://pbs.twimg.com/media/DvwQQLnWwAEBXmY?format=jpg&name=small",
+		},
+		{
+			position: 8,
+			artist: "Jeff Rosenstock",
+			name: "POST-",
+			cover:
+				"https://pbs.twimg.com/media/DvwPzsHX4AIFdix?format=jpg&name=medium",
+		},
+		{
+			position: 9,
+			artist: "Charlie Barnes",
+			name: "Oceanography",
+			cover:
+				"https://pbs.twimg.com/media/DvwPa-2XgAIqpNs?format=jpg&name=900x900",
+		},
+		{
+			position: 10,
+			artist: "Varry Brava",
+			name: "Furor",
+			cover:
+				"https://pbs.twimg.com/media/DvwO-rqWoAAZoYO?format=jpg&name=medium",
+		},
+		{
+			position: 11,
+			artist: "Arctic Monkeys",
+			name: "Tranquility Base Hotel & Casino",
+			cover:
+				"https://pbs.twimg.com/media/DvwOkusXcAEkX6S?format=jpg&name=small",
+		},
+		{
+			position: 12,
+			artist: "Gorillaz",
+			name: "The Now Now",
+			cover:
+				"https://indiehoy.com/wp-content/uploads/2018/06/gorillaz-the-now-now-album-cover-1200x1200.jpg",
+		},
+		{
+			position: 13,
+			artist: "IZAL",
+			name: "Autoterapia",
+			cover:
+				"https://cdns-images.dzcdn.net/images/cover/ab80cd41cc72675224652d906227dcac/500x500.jpg",
+		},
+		{
+			position: 14,
+			artist: "Noah Cyrus",
+			name: "Good Cry",
+			cover:
+				"https://pbs.twimg.com/media/DvwNeBgX4AAKdQ-?format=jpg&name=medium",
+		},
+		{
+			position: 15,
+			artist: "Dorian",
+			name: "Justicia Universal",
+			cover:
+				"https://pbs.twimg.com/media/DvwNDTNX4AEkz7k?format=jpg&name=900x900",
+		},
+		{
+			position: 16,
+			artist: "Khruangbing",
+			name: "Con Todo El Mundo",
+			cover:
+				"https://pbs.twimg.com/media/DvwMe4xWsAARN9r?format=jpg&name=medium",
+		},
+		{
+			position: 17,
+			artist: "Boy Pablo",
+			name: "Soy Pablo",
+			cover:
+				"https://pbs.twimg.com/media/DvwMGdpWwAElj7I?format=jpg&name=medium",
+		},
+		{
+			position: 18,
+			artist: "Chvrches",
+			name: "Love Is Dead",
+			cover: "https://m.media-amazon.com/images/I/913Fh9rklgL._SL1500_.jpg",
+		},
+		{
+			position: 19,
+			artist: "Mumford & Sons",
+			name: "Delta",
+			cover:
+				"https://pbs.twimg.com/media/DvwLRG0X0AITTR2?format=jpg&name=small",
+		},
+		{
+			position: 20,
+			artist: "Natalia Lafourcade",
+			name: "Musas Vol. II",
+			cover:
+				"https://pbs.twimg.com/media/DvwK9dfWoAAYOJN?format=jpg&name=medium",
+		},
+	],
+};
+
+export const seventeen = {
+	year: "2017",
+	albums: [
+		{
+			position: 1,
+			artist: "Arcade Fire",
+			name: "Everything Now",
+			cover:
+				"https://pbs.twimg.com/media/DRmY1qYXcAAJRYb?format=jpg&name=small",
+		},
+		{
+			position: 2,
+			artist: "Phoenix",
+			name: "Ti Amo",
+			cover:
+				"https://pbs.twimg.com/media/DRmZUXPWkAElxlH?format=jpg&name=medium",
+		},
+		{
+			position: 3,
+			artist: "Taylor Swift",
+			name: "reputation",
+			cover:
+				"https://pbs.twimg.com/media/DRmYeS9WsAMhSXD?format=jpg&name=small",
+		},
+		{
+			position: 4,
+			artist: "The xx",
+			name: "I See You",
+			cover:
+				"https://i2.wp.com/estacionindierock.com/wp-content/uploads/2018/11/the-xx-i-see-you.jpg?w=1080&ssl=1",
+		},
+		{
+			position: 5,
+			artist: "Gorillaz",
+			name: "Humanz",
+			cover:
+				"https://pbs.twimg.com/media/DRmXqyGXcAEwhg3?format=jpg&name=small",
+		},
+		{
+			position: 6,
+			artist: "La M.O.D.A.",
+			name: "Salvavida (de las balas perdidas)",
+			cover:
+				"https://pbs.twimg.com/media/DRmXTA1XkAAQmvx?format=jpg&name=medium",
+		},
+		{
+			position: 7,
+			artist: "Royal Blood",
+			name: "How Did We Get So Dark",
+			cover:
+				"https://pbs.twimg.com/media/DRmW-XHXcAAR9d0?format=jpg&name=small",
+		},
+		{
+			position: 8,
+			artist: "Francesco Gabbani",
+			name: "Magellano",
+			cover:
+				"https://i.discogs.com/nIMbRPHS62YUqBHEpMf8jFsByK3TI0ubEK1PWw9LRYA/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEwMjA1/NjA4LTE1MTI3OTA0/NjctODc2Mi5qcGVn.jpeg",
+		},
+		{
+			position: 9,
+			artist: "Anathema",
+			name: "The Optimist",
+			cover: "https://m.media-amazon.com/images/I/71a-1BmMtIL._SL1500_.jpg",
+		},
+		{
+			position: 10,
+			artist: "Cashmere Cat",
+			name: "9",
+			cover:
+				"https://lastfm.freetls.fastly.net/i/u/770x0/90c8d028f4b24f6e62c7c3eb901d82e6.jpg",
+		},
+	],
+};
---
+++ src/tops/MusicTops.js
@@ -0,0 +1,27 @@
+import "./MusicTops.css";
+import YearRow from "./music/YearRow";
+import {
+	twentyOne,
+	twenty,
+	nineteen,
+	eighteen,
+	seventeen,
+} from "../constants/musicTops";
+import Navbar from "../Navbar";
+
+const MusicTops = () => {
+	return (
+		<>
+			<Navbar />
+			<div className="tops-containers">
+				<YearRow yearClass="twenty-one" albumList={twentyOne} />
+				<YearRow yearClass="twenty" albumList={twenty} />
+				<YearRow yearClass="nineteen" albumList={nineteen} />
+				<YearRow yearClass="eighteen" albumList={eighteen} />
+				<YearRow yearClass="seventeen" albumList={seventeen} />
+			</div>
+		</>
+	);
+};
+
+export default MusicTops;
